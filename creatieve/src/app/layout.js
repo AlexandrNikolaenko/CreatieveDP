fcd94fa5f4569f480d608fc4d41bcbf785b51d69
Александр <nikol.alex06@mail.ru>
2025-01-16 19:41:25 +0300

    Fix metadata

--- creatieve/src/app/layout.js
+++ creatieve/src/app/layout.js
@@ -4,12 +4,14 @@ export const metadata = {
   title: "Разработка недорогих сайтов под ключ в СПб",
   description: "Мы команда разработчиков CREATIEVE занимаемся разработкой и продвижением сайтов и веб-приложений различной сложности под ключ по ценам ниже рынка",
   keywords: "разработка сайта под ключ, купить сайт санкт петербург, сайт недорого, заказать сайт под ключ, сайт под ключ недорого, заказать сайт кодом, заказать сайт компании, одностраничный сайт под ключ, заказать разработку сайта под ключ, разработка сайта под ключ недорого, web сайт под ключ",
-  robots: "index,follow",
-  author: "Alexander, nikol.alex06@mail.ru",
-  replyTo: "nikol.alex06@mail.ru",
-  url: "https://creatieve.ru",
-  identifierURL: "https://creatieve.ru",
-  yandex_site_verification: "8df417d384e5203c"
+  other: {
+    ['robots']: "index,follow",
+    ['author']: "Alexander, nikol.alex06@mail.ru",
+    ['replyTo']: "nikol.alex06@mail.ru",
+    ['url']: "https://creatieve.ru",
+    ['identifierURL']: "https://creatieve.ru",
+    ['yandex_site_verification']: "8df417d384e5203c"
+  }
 };
 
 export default function RootLayout({ children }) {
