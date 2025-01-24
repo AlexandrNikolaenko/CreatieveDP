fe10809f4d1103fb1e41c652897ea3b2b69da0d7
Александр <nikol.alex06@mail.ru>
2025-01-24 20:53:18 +0300

    fix commit

--- creatieve/src/app/global-error.js
+++ creatieve/src/app/global-error.js
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 export default function GlobalError({ error, reset }) {
   useEffect(() => {
     // Log the error to an error reporting service
-    console.error(error)
+    alert(error)
   }, [error])
 
   return (
